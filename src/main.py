742a31862f960e10072150a5c1fcaaff4d23e3a7
Dorian DE ROSA <dorian.de-rosa@epita.fr>
2021-09-29 21:48:31 +0200

    Added name, count (not working), admin, mute, unmute, ban commands

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import os
 from discord.ext import commands
+from discord.utils import get
+import discord
+
+intents = discord.Intents(messages=True, guilds=True, members=True)
 
 bot = commands.Bot(
     command_prefix="!",  # Change to desired prefix
-    case_insensitive=True  # Commands aren't case-sensitive
+    case_insensitive=True,  # Commands aren't case-sensitive
+    intents=intents
 )
 
-bot.author_id = 892823067074633730  # Change to your discord id!!!
+bot.author_id =   # Change to your discord id!!!
 
 @bot.event
 async def on_ready():  # When the bot is ready
@@ -17,5 +22,72 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "ODkyODIyNjE2NzY1NzkyMjc2.YVSf2w.vk7Sdx_zxaqb7DGlZnuP10XmgJ0"
+@bot.command()
+async def name(ctx):
+    await ctx.send(ctx.author)
+
+@bot.command()
+async def count(ctx):
+    online = []
+    offline = []
+    idle = []
+    dnd = []
+    invisible = []
+    members = await ctx.guild.fetch_members().flatten()
+    print(members)
+    for member in members:
+        print(member.name)
+        print(member.status)
+        if member.status == 'online':
+            online.append(member.name)
+        if member.status == 'offline':
+            offline.append(member.name)
+        if member.status == 'idle':
+            idle.append(member.name)
+        if member.status == 'dnd' or member.status == 'do_not_disturb':
+            dnd.append(member.name)
+        if member.status == 'invisible':
+            invisible.append(member.name)
+    print(online)
+    print(offline)
+    print(idle)
+    print(dnd)
+    print(invisible)
+
+@bot.command()
+async def admin(ctx, member: discord.Member):
+
+    if not get(ctx.guild.roles, name='Admin'):
+        perms = discord.Permissions(kick_members=True, ban_members=True, manage_channels=True)
+        await ctx.guild.create_role(name='Admin', permissions=perms)
+    
+    role = get(ctx.guild.roles, name='Admin')
+    await member.add_roles(role)
+
+@bot.command()
+async def mute(ctx, member: discord.Member):
+    if not get(ctx.guild.roles, name='Unmuted'):
+        perms = discord.Permissions(send_messages=True)
+        await ctx.guild.create_role(name='Unmuted', permissions=perms)
+    
+    role = get(ctx.guild.roles, name='Unmuted')
+    if role in member.roles:
+        await member.remove_roles(role)
+
+@bot.command()
+async def unmute(ctx, member: discord.Member):
+    if not get(ctx.guild.roles, name='Unmuted'):
+        perms = discord.Permissions(send_messages=True)
+        await ctx.guild.create_role(name='Unmuted', permissions=perms)
+    
+    role = get(ctx.guild.roles, name='Unmuted')
+    if not role in member.roles:
+        await member.add_roles(role)
+
+@bot.command()
+async def ban(ctx, member: discord.Member):
+    await member.ban()
+
+
+token = ""
 bot.run(token)  # Starts the bot
